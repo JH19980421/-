2ab8db0ab80e746ed24af4d2778ccdcfa4413427
JH19980421 <choijongho2@gmail.com>
2023-08-21 20:04:39 +0900

    로그인 페이지 복구

--- demo/src/main/java/com/example/demo/web/security/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/web/security/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .antMatchers("/app/boards/home").permitAll()
+                        .antMatchers("/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
