21aa7c98cf2b214ccb4841781d3b5e9664fb9635
JH19980421 <choijongho2@gmail.com>
2023-08-21 20:50:52 +0900

    [add] 스프링 시큐리티 사용자 & 비밀번호 로그인 조회기능 추가

---
+++ demo/src/main/java/com/example/demo/api/service/UserSecurityService.java
@@ -0,0 +1,40 @@
+package com.example.demo.api.service;
+
+import com.example.demo.api.repository.UserRepository;
+import com.example.demo.web.entity.BaseEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class UserSecurityService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> checkUser = userRepository.findByEmailAndState(username, BaseEntity.State.ACTIVE);
+        if (checkUser.isEmpty()) {
+            throw new UsernameNotFoundException("사용자를 찾을수 없습니다.");
+        }
+        User user = checkUser.get();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+//        if ("admin".equals(username)) {
+//            authorities.add(new SimpleGrantedAuthority(User.UserRole.ADMIN.getValue()));
+//        } else {
+//            authorities.add(new SimpleGrantedAuthority(User.UserRole.USER.getValue()));
+//        }
+
+        return new User(user.getUsername(), user.getPassword(), authorities);
+    }
+}
