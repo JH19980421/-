be83782355599bf04007a5ed430ab79f8a4d3e4f
JH19980421 <choijongho2@gmail.com>
2023-08-21 19:54:03 +0900

    [modify] 로그인 화면 변경

--- demo/src/main/java/com/example/demo/web/security/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/web/security/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers().permitAll()
+                        .antMatchers("/app/boards/home").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
